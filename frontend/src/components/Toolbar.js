5fd224732f09eb91e2d6deb171fe9c539a6a8d5d
EliWoodard <elijahwoodard136@gmail.com>
2025-02-06 14:28:44 -0700

    Added Players

--- frontend/src/components/Toolbar.js
+++ frontend/src/components/Toolbar.js
@@ -37,23 +37,59 @@ const Toolbar = ({ addTile, addCircle, selectPlayer, toggleDropdown }) => {
 
             <div id="dropdownMenu" className="dropdown-menu hidden">
                 {/* Wrap calls in arrow functions */}
-                <div
-                    className="dropdown-option"
-                    onClick={() => selectPlayer("Gandalf")}
-                >
+                <div className="dropdown-option" onClick={() => selectPlayer("Gandalf")}>
                     Gandalf
                 </div>
-                <div
-                    className="dropdown-option"
-                    onClick={() => selectPlayer("Option 2")}
-                >
-                    Option 2
+                <div className="dropdown-option" onClick={() => selectPlayer("Beorn")}>
+                    Beorn
                 </div>
-                <div
-                    className="dropdown-option"
-                    onClick={() => selectPlayer("Option 3")}
-                >
-                    Option 3
+                <div className="dropdown-option" onClick={() => selectPlayer("TheGreatBear")}>
+                    The Great Bear
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Freahild")}>
+                    Freahild
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("CalaminthTook")}>
+                    Calaminth Took
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Arwen")}>
+                    Arwen
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Boromir")}>
+                    Boromir
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Bilbo")}>
+                    Bilbo
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Beravor")}>
+                    Beravor
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Renerien")}>
+                    Renerien
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Eleanor")}>
+                    Eleanor
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Elena")}>
+                    Elena
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Aragorn")}>
+                    Aragorn
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Legolas")}>
+                    Legolas
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Gimli")}>
+                    Gimli
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Dwalin")}>
+                    Dwalin
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Dis")}>
+                    Dis
+                </div>
+                <div className="dropdown-option" onClick={() => selectPlayer("Ballin")}>
+                    Ballin
                 </div>
             </div>
         </div>
